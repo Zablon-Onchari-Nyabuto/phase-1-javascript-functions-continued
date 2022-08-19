2cf50dfc17002cf11081add2673b159ca0dd68bb
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-08-19 10:09:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+
+function mondayWork(option = "go to the office") {
+  return `This Monday, I will ${option}.`
+}
+mondayWork();
+
+function wrapAdjective(action = "*") {
+  return function (parameter = "special") {
+    return `You are ${action}${parameter}${action}!`
+
+  }
+}
+wrapAdjective()('a hardworker');
